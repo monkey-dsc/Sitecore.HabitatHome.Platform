8983ca446a0ebc9bb30f1cd94b37e8c276775612
dsc <dsc@indeca.de>
2019-05-10 17:52:35 +0200

    - reserializing
    - added forms and emails

---
+++ src/Feature/MarketingAutomation/code/Models/TestDriveModel.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.HabitatHome.Feature.MarketingAutomation.Facets;
+using Sitecore.XConnect;
+using Sitecore.XConnect.Collection.Model;
+using Sitecore.XConnect.Schema;
+
+namespace Sitecore.HabitatHome.Feature.MarketingAutomation.Models
+{
+    public class TestDriveModel
+    {
+        public static XdbModel Model { get; } = BuildModel();
+
+        private static XdbModel BuildModel()
+        {
+            var modelBuilder = new XdbModelBuilder("TestDriveModel", new XdbModelVersion(0, 1));
+
+            modelBuilder.ReferenceModel(CollectionModel.Model);
+            modelBuilder.DefineFacet<Contact, TestDrive>(TestDrive.DefaultFacetKey);
+
+            return modelBuilder.BuildModel();
+        }
+    }
+}
---
+++ src/Foundation/ModelDeployment/code/Sitecore.XConnect.ModelDeployment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Sitecore.HabitatHome.Feature.MarketingAutomation.Models;
+using Sitecore.XConnect.Serialization;
+
+namespace Sitecore.XConnect.ModelDeployment
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Serialize();
+        }
+
+        public static void Serialize()
+        {
+            var model = XdbModelWriter.Serialize(TestDriveModel.Model);
+            File.WriteAllText(TestDriveModel.Model.FullName + ".json", model);
+            Console.WriteLine("Completed");
+            Console.Read();
+        }
+    }
+}
